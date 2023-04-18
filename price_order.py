b5af8401934a568322368b51025b39a6422f8403
FauciLee <694063265@qq.com>
2023-04-18 18:15:02 +0800

    李福奇-

--- price_order.py
+++ price_order.py
@@ -49,14 +49,54 @@ def analyse_price_order():
     # plt.show()
 def analyse_region_order():
     data = pd.read_csv("数据/order_train1.csv")
-    #首先分析各个销售区域的订单量（DataAnalysis文件分析的是数据行数，而不是订单量）（柱状图）
-    data.sales_region_code.value_counts().plot(kind="bar")
-    plt.title("sales_region_code")
+    region_list=data.groupby("sales_region_code")
+    map = {}
+    for value,group in region_list:
+        map[value]=group["ord_qty"].sum()
+    #首先分析各个销售区域的订单量加和（柱状图）
+    plt.bar(map.keys(),map.values())
+    plt.title("analyze sales_region_code with ord_qty_sum by bar")
+    plt.xlabel("sales_region_code")
+    plt.ylabel("ord_qty_sum")
     plt.show()
-    #分析各个销售区域的订单量占比（饼状图）
-    data.sales_region_code.value_counts().plot(kind="pie")
-    plt.title("sales_region_code")
+    #分析各个销售区域的订单量加和（饼状图）
+    plt.pie(map.values(),labels=map.keys(),autopct="%1.1f%%")
+    plt.title("analyze sales_region_code with ord_qty_sum by pie")
+    plt.xlabel("sales_region_code")
+    plt.ylabel("ord_qty_sum")
     plt.show()
+    #分析各个销售区域的订单量加和（折线图）
+    category101_map=  {}
+    category102_map = {}
+    category103_map = {}
+    category104_map = {}
+    category105_map = {}
+    for value,group in region_list:
+        tmp=group.groupby("first_cate_code")
+        for value1,group1 in tmp:
+            if value==101:
+                category101_map[value1]=group1["ord_qty"].sum()
+            if value==102:
+                category102_map[value1]=group1["ord_qty"].sum()
+            if value==103:
+                category103_map[value1]=group1["ord_qty"].sum()
+            if value==104:
+                category104_map[value1]=group1["ord_qty"].sum()
+            if value==105:
+                category105_map[value1]=group1["ord_qty"].sum()
+    plt.plot(category101_map.keys(),category101_map.values(),label="101")
+    plt.plot(category102_map.keys(), category102_map.values(), label="102")
+    plt.plot(category103_map.keys(), category103_map.values(), label="103")
+    plt.plot(category104_map.keys(), category104_map.values(), label="104")
+    plt.plot(category105_map.keys(), category105_map.values(), label="105")
+    plt.legend()
+    plt.title("analyze first_cate_code with ord_qty_sum by line")
+    plt.xlabel("sales_region_code")
+    plt.ylabel("ord_qty_sum")
+    plt.show()
+
+
+
 
 if __name__=="__main__":
   analyse_region_order()
