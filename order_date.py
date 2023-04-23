e63117be3cc36370a4fd5d77bec3d4ae4948aa86
Lilchai <913417494@qq.com>
2023-04-23 17:37:26 +0800

    按不同年份分析了第五题

--- order_date.py
+++ order_date.py
@@ -21,11 +21,27 @@ def classify_by_year(data):
     demand_by_time.plot(kind='bar')
     plt.show()
 
+def classify_by_everyyear(data):
+    data['order_date'] = pd.to_datetime(data['order_date'])
+    data['year'] = data['order_date'].dt.year
+    data_every_year = []
+    for year in range(2015, 2019):
+        data_year = data[data['year'] == year]
+        data_year['order_date'] = pd.to_datetime(data_year['order_date']).dt.month
+        demand_by_time = data_year.groupby('order_date')['ord_qty'].sum()
+
+        # 绘制柱状图
+        plt.title(str(year))
+        demand_by_time.plot(kind='bar')
+        plt.show()
+        data_every_year.append(data_year)
+
 if __name__ == '__main__':
     # 读取csv文件
     data = pd.read_csv('数据/order_train1.csv')
 
-    classify_by_month(data)
-    classify_by_year(data)
+    # classify_by_month(data)
+    # classify_by_year(data)
+    classify_by_everyyear(data)
 
 
